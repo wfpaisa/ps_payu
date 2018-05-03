79228f0641b996ddafaa899b0400b3649cb8de37
Felipe Uribe <wfpaisa@gmail.com>
2018-05-03 14:09:19 -0500

    Add translations

---
+++ translations/cb.php
@@ -0,0 +1,9 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{ps_payu}prestashop>ps_payu_6a90d0ab04a2cd28919bee487736cdeb'] = 'PayU';
+$_MODULE['<{ps_payu}prestashop>ps_payu_e60368234dcbc533f32b54dc69606c15'] = 'Pago Webcheckout con PayU Latam';
+$_MODULE['<{ps_payu}prestashop>ps_payu_a02758d758e8bec77a33d7f392eb3f8a'] = 'No se ha fijado ninguna moneda para este módulo.';
+$_MODULE['<{ps_payu}prestashop>ps_payu_259b165e82378014a5b167d05f1a957e'] = 'Pago en línea con tarjeta de crédito, débito, PSE o en efectivo';
+$_MODULE['<{ps_payu}prestashop>hook_payment_option_detail_1ef284d76c6e65cf659672022b0e4a9e'] = 'Una vez realices el pedido podrás pagar utilizando el método de pago de tu preferencia desde PayU.';
